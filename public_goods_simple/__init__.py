b1277022da0a436af4ff7239436eaa596fe677d6
Llampi <ll_elias@hotmail.com>
2023-10-20 00:15:11 -0500

    arreglo de errores y creacion de frames

--- public_goods_simple/__init__.py
+++ public_goods_simple/__init__.py
@@ -10,10 +10,28 @@ class C(BaseConstants):
     NAME_IN_URL = 'public_goods_simple'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
-    ENDOWMENT = 500
+    ENDOWMENT = 100
+    
+
 
 
 class Subsession(BaseSubsession):
+    '''
+    def creating_session(self):
+        players = self.get_players()
+        num_players = len(players)
+        for player in players:
+            if player.id_in_group == num_players:
+                player.participant.vars['rol'] = 'Funcionario'
+            else:
+                player.participant.vars['rol'] = 'Ciudadano ' + str(player.id_in_group)
+                '''
+    
+    def creating_session(self):
+        players = self.get_players()
+        for player in (players):
+                player.rol = '22'
+    
     pass
 
 
@@ -23,21 +41,37 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     BasePlayer.payoff = C.ENDOWMENT
+    endowment_izquierda = models.IntegerField(initial=C.ENDOWMENT)
+    endowment_derecha = models.IntegerField(initial=C.ENDOWMENT)
+    transferencia_neta = models.IntegerField(initial=0)
+
     pass
 
 
 # PAGES
 class Bargain(Page):
+
     
+    endowment_izquierda = C.ENDOWMENT;
+    endowment_derecha = C.ENDOWMENT;
+
+    @staticmethod
+    
+
     @staticmethod
     def vars_for_template(player):
 
         numero_de_participantes = player.session.num_participants
-        lista_de_ciudadanos = [] # de 1 n, donde n es el numero de ciudadanos
+        lista_de_ciudadanos_y_nicknames = [] # de 1 n, donde n es el numero de ciudadanos
         lista_de_participantes = [] # de 1 a n-1, donde n es el numero de ciudadanos
 
         for i in range(1, numero_de_participantes):
-            lista_de_ciudadanos.append(i)
+            if(i == numero_de_participantes):
+                nickname = "Funcionario"
+            else:
+                nickname = "Ciudadano " + str(i)
+            nuevo_diccionario = {"id": i, "nickname": nickname}
+            lista_de_ciudadanos_y_nicknames.append(nuevo_diccionario)
         for i in range(1, numero_de_participantes+1):
             lista_de_participantes.append(i)
 
@@ -57,18 +91,29 @@ class Bargain(Page):
 
         id_de_funcionario = numero_de_participantes
 
+        nicknames = []
+        for i in range(1, numero_de_participantes+1):
+            if(i == numero_de_participantes):
+                nickname = "Funcionario"
+            else:
+                nickname = "Ciudadano " + str(i)
+            nuevo_diccionario = {"id": i, "nickname": nickname}
+            nicknames.append(nuevo_diccionario)
         #puntos iniciales de participantes:
         #puntos = []
         #for i in range(1,numero_de_participantes):
         #    nuevo_diccionario = {"user1": i, "user2": j, "nombre_canal_chat": str(i)+str(j)}
-
+        
 
 
         return dict(
             id_de_funcionario = id_de_funcionario,
-            lista_de_ciudadanos = lista_de_ciudadanos,
+            lista_de_ciudadanos_y_nicknames = lista_de_ciudadanos_y_nicknames,
             lista_de_participantes = lista_de_participantes,
             chat_grupos = chat_grupos,
+            nicknames = nicknames,
+            #endowment_izquierda = player.endowment_izquierda,
+            #endowment_derecha = player.endowment_izquierda,
         )
 
 
@@ -77,9 +122,39 @@ class Bargain(Page):
 
     @staticmethod
     def live_method(player, data):
-        print('received',  data)
-        #if(data['operacion'] == 'proyecto_comun'):
-        #    data['monto_comun'] == data['monto_comun'] + data['puntos']
+        
+
+        # estructura al enviar datos con live send:
+        '''
+        -'emisor': emisor (int), 
+        -'receptor': receptor (int),  
+        -'operacion':'negociar_puntos',        
+            -'sub_operacion': 'enviar_puntos',
+                            -'puntos' : (int)
+            -'sub_operacion':'enviar_puntos_respuesta'    
+                            -'respuesta' : 'si'
+                            -'respuesta' : 'no'        
+            -'sub_operacion'. 'cancelar_enviar_puntos'
+            -'sub_operacion'. 'solicitar_puntos'
+                            -'puntos' : (int)
+            -'sub_operacion'. 'solicitar_puntos_respuesta'
+                            -'respuesta' : 'si'
+                            -'respuesta' : 'no' 
+            -'sub_operacion'. 'cancelar_solicitar_puntos'
+
+        -'operacion'. 'proyecto_comun'
+            -'sub_operacion': 'ciudadano_envia_puntos'
+                            - 'puntos' : (int)
+            -'sub_operacion': 'funcionario_envia_puntos'
+                            - 'puntos' : (int)
+
+        ejem: liveSend({'emisor': parseInt(emisor), 'receptor': parseInt(receptor),  'operacion':'negociar_puntos', 'sub_operacion': 'solicitar_puntos','puntos': 44});
+        '''
+
+
+        if(data['sub_operacion'] == 'enviar_puntos_respuesta'):
+            print("actualizar puntos: " + str(data['puntos']))
+        
         return {data['receptor']: data}
     pass
 
